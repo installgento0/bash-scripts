5a2ae0322d09f4307868cfe033a9265c49363cd0
Nicolas <32076027+installgento0@users.noreply.github.com>
2019-06-21 01:30:41 -0300

    Update clonepurge.sh

--- clonepurge.sh
+++ clonepurge.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if find $(pwd) -mindepth 1 | read; then
 echo "Folder not empty"
-for i in $(gmd5sum * | sort | awk '{ print $2, $1 }' | guniq -df 1 | awk '{ print $1 }' ); do rm $i.*; done
+for f in *\ *; do mv "$f" "${f// /_}"; done
+for i in $(shasum * | sort | awk '{ print $2, $1 }' | guniq -df 1 | awk '{ print $1 }' ); do rm $i; done
 else
 echo "Folder empty"
 fi
